a1e9b8b0e67e78cdfefa266a21693a04d14cde98
druckmax <max_sommerfeld@web.de>
2022-05-17 15:46:38 +0200

    fixed displayed total amt of change to 2 decimals

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const paidMoney = +prompt("Enter the given money: ")
 function calcChange(price, paidMoney) {
   // Calculate change
   let change = paidMoney - price;
-  console.log(`The total change is: ${change}`);
+  console.log(`The total change is: ${change.toFixed(2)}`);
   // Setting array of all possible options for the change
   const moneyUnits = [
     500, 200, 100, 50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01,
