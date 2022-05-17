761ff50d15a2a8c6f9e73b622f7bfbf9b5e8049e
druckmax <max_sommerfeld@web.de>
2022-05-17 13:22:09 +0200

    added comments

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const prompt = require("prompt-sync")({ sigint: true });
 
-// const paidMoney = 50;
-// const price = 3.77;
-
 const price = +prompt("Please enter the price: ");
 const paidMoney = +prompt("Enter the given money: ")
 
@@ -10,6 +7,7 @@ function calcChange(price, paidMoney) {
   // Calculate change
   let change = paidMoney - price;
   console.log(`The total change is: ${change}`);
+  // Setting array of all possible options for the change
   const moneyUnits = [
     500, 200, 100, 50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01,
   ];
@@ -17,10 +15,11 @@ function calcChange(price, paidMoney) {
   // Iteration
   let counterObj = {};
   for (let unit of moneyUnits) {
+      // Dividing 'change' with each item from the 'moneyUnits' array and check if the result is larger than 1, meaning we can use the unit for the change. If so, add the result to the counterObj object. The iteration unit from the array becomes the key in the counterObject and the result is saved as its value. Then calculate the remainder of change and the respective unit and set it as 'change' for the next iteration.
     let division = change / unit;
     if (division >= 1) {
       counterObj[unit] = Math.floor(division);
-      change = ((change % unit)*100).toFixed(3)/100;
+      change = ((change % unit)*100).toFixed(2)/100;
 
       // Extra conditions in case you don't need to give change or the customer has to pay more
     } else if (price === paidMoney) {
@@ -30,12 +29,14 @@ function calcChange(price, paidMoney) {
     }
   }
   // Sorting final object and returning each key-value to console.
+  //1. Mutating to object-keys array
+  //2. Sorting from small to big
+  //3. Print each value of the respective key to the console
   console.log("You need to return:");
   return Object.keys(counterObj)
     .sort(function (a, b) {
-      return a - b;
+      return b-a;
     })
-    .reverse()
     .forEach(function (i) {
       console.log(`${counterObj[i]} x ${i}€`);
     });
